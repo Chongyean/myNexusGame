2c0452be4108b1c4e99c766059dcd5d792710fc4
Chongyean <taingchongyean@gmail.com>
2025-02-07 18:39:18 +0700

    Finish GetStarted session at 6: 39pm

---
+++ src/components/GetStarted/GetStarted.jsx
@@ -0,0 +1,22 @@
+import "./GetStarted.css";
+const GetStarted = () => {
+  return (
+    <section className="g-wrapper">
+      <div className="paddings innerWidth g-container">
+        <div className="flexColCenter inner-container">
+          <span className="primaryText">Get Started with Homyz</span>
+          <span className="secondaryText">Find super attractive price quotes from us.</span>
+          <span>
+            <br />
+            Find your residence soon
+          </span>
+          <button className="button">
+            <a href="taingchongyean@gmail.com">Get Started</a>
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default GetStarted;
